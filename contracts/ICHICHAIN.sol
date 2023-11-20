f8101b56a3dc5df87648f1c643e2c109d55b7855
Chen-Hsien <zxcvb1088@gmail.com>
2023-11-20 09:44:16 +0800

    V1 with series, prize structure, mint function

--- contracts/ICHICHAIN.sol
+++ contracts/ICHICHAIN.sol
@@ -1,52 +1,131 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "erc721a/contracts/ERC721A.sol";
 
-contract MyNFT is ERC721, Ownable {
-    struct NFT {
-        uint256 totalQuantity;
-        uint256 remainingQuantity;
-        uint256[] prizeQuantity;
+contract ICHICHAIN is ERC721A, Ownable {
+    struct Prize {
         string prizeName;
+        uint256 prizeRemainingQuantity;
+    }
+
+    struct Series {
+        Prize[] seriesPrizes;
+        string seriesName;
+        uint256 totalTicketNumbers;
+        uint256 remainingTicketNumbers;
         uint256 price;
-        uint256 id;
         uint256 revealTime;
+        string baseURI;
     }
 
-    mapping(uint256 => NFT) public nfts;
+    mapping(uint256 => Series) public ICHISeries;
+    mapping(uint256 => uint256) private tokenSeriesMapping;
 
-    constructor() ERC721("ICHICHAIN", "ICHI") {}
+    string private _blindTokenURI =
+        "https://gateway.pinata.cloud/ipfs/QmRx7xyp6o6Rc9XvQiMRjjJH8K9BAssxv8kvXEjqdaZi2z/";
 
-    function createNFT(
-        uint256 tokenId,
-        uint256 totalQuantity,
-        uint256[] memory prizeQuantity,
-        string memory prizeName,
-        uint256  price,
-        uint256 revealTime
+    constructor() ERC721A("ICHICHAIN", "ICHI") {}
+
+    uint256 private seriesCounter = 0; // 新增內部計數器
+
+    function createSeries(
+        string memory seriesName,
+        uint256 totalTicketNumbers,
+        uint256 price,
+        uint256 revealTime,
+        string memory baseURI,
+        Prize[] memory prizes
     ) public onlyOwner {
-        uint256 remainingQuantity = totalQuantity;
-        NFT memory newNFT = NFT(totalQuantity,remainingQuantity, prizeQuantity, prizeName,price , tokenId, revealTime);
-        nfts[tokenId] = newNFT;
+        uint256 seriesID = seriesCounter++; // 自動生成seriesID
+        Series storage series = ICHISeries[seriesID];
+        series.seriesName = seriesName;
+        series.totalTicketNumbers = totalTicketNumbers;
+        series.remainingTicketNumbers = totalTicketNumbers;
+        series.price = price;
+        series.revealTime = revealTime;
+        series.baseURI = baseURI;
+
+        for (uint256 i = 0; i < prizes.length; i++) {
+            series.seriesPrizes.push(prizes[i]);
+        }
     }
 
-    function AdminMint(address to, uint256 tokenId) public onlyOwner {
-        _safeMint(to, tokenId);
+    function getSeriesPrizes(uint256 seriesID)
+        public
+        view
+        returns (Prize[] memory)
+    {
+        return ICHISeries[seriesID].seriesPrizes;
     }
 
-     function mint (address to, uint256 tokenId) public payable{
-        // require( nfts[tokenId].totalQuantity > 0, "NFT is not exist");
-        require( nfts[tokenId].remainingQuantity > 0, "Series is sold out");
-        require( msg.value > nfts[tokenId].price , "Not enough money");
-        _safeMint(to, tokenId);
+    function getSeriesId(uint256 tokenId) public view returns (uint256) {
+        require(_exists(tokenId), "Token does not exist");
+        return tokenSeriesMapping[tokenId];
     }
 
-    function reveal(uint256 tokenId) public onlyOwner {
-        require( nfts[tokenId].revealTime > block.timestamp, "NFT not in reveal time");
-        nfts[tokenId].revealTime = block.timestamp;
+    function mint(uint256 seriesID, uint256 quantity) public payable {
+        Series storage series = ICHISeries[seriesID];
+
+        // Check if the series exists
+        require(series.seriesPrizes.length > 0, "Series does not exist");
+
+        // The total price for minting the specified quantity
+        uint256 totalPrice = series.price * quantity;
+
+        // Check if the sent Ether matches the total price for requested quantity
+        require(msg.value >= totalPrice, "Insufficient funds sent");
+
+        // Check if there are enough remaining NFTs in the series
+        require(
+            quantity <= series.remainingTicketNumbers,
+            "Not enough NFTs remaining in the series"
+        );
+
+        // Store the starting index for the new tokens
+        uint256 newTokenIdStart = _nextTokenId();
+
+        // Mint the specified quantity of NFTs to the sender's address in one call
+        _safeMint(msg.sender, quantity);
+
+        // Update the mapping for each newly minted token
+        for (uint256 i = 0; i < quantity; i++) {
+            tokenSeriesMapping[newTokenIdStart + i] = seriesID;
+        }
+
+        series.remainingTicketNumbers -= quantity;
+
+        // Optionally, handle overpayment
+        if (msg.value > totalPrice) {
+            payable(msg.sender).transfer(msg.value - totalPrice);
+        }
     }
+
+    function AdminMint(
+        address to,
+        uint256 seriesID,
+        uint256 quantity
+    ) public onlyOwner {
+        Series storage series = ICHISeries[seriesID];
+
+        // Check if there are enough remaining NFTs in the series
+        require(
+            quantity <= series.remainingTicketNumbers,
+            "Not enough NFTs remaining in the series"
+        );
+
+        _safeMint(to, quantity);
+
+        series.remainingTicketNumbers -= quantity;
+    }
+
+    // function reveal(uint256 seriesID) public onlyOwner {
+    //     require( nfts[seriesID].revealTime > block.timestamp, "NFT not in reveal time");
+    //     nfts[seriesID].revealTime = block.timestamp;
+    // }
 
     // function reveal
+
+    // function exchangePrize
 }
